ce3cbdf8b40ba943c675522d6bdde1418e53cbcb
Doc Ritezel and Rae Bonfanti <pair+doc+rae@ministryofvelocity.com>
2014-05-31 14:32:16 -0700

    passing convert

--- convert.c
+++ convert.c
@@ -1,1 +1,6 @@
-int main (){ return 0; }
+#include <stdio.h>
+
+int main (){
+  fprintf(stdout, "{\"index\": {\"_index\": \"wat\", \"_type\": \"taco\", \"_id\": \"3\", \"_parent\": \"4\"}}\n{\"user_id\": \"5\", \"collection_id\": \"6\"}");
+  return 0;
+}
